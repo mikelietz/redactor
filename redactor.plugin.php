9e17a1c9015c5e57ac56b0d0d9495b16f94d6ad8
ringmaster <a_github@midnightcircus.com>
2012-07-27 19:58:07 -0400

    Allow insertion of images from the media silo.

--- redactor.plugin.php
+++ redactor.plugin.php
@@ -12,7 +12,16 @@ class Redactor extends Plugin {
 			Stack::add( 'admin_header_javascript', $this->get_url() . '/redactor/redactor.min.js', 'redactor', 'jquery' );
 			Stack::add( 'admin_stylesheet', array( $this->get_url() . '/redactor/css/redactor.css', 'screen' ), 'redactor', 'admin-css' );
 
-			$js = "$(document).ready( function(){ $('#content').redactor(); });";
+			$js = <<< REDACTOR_INIT_JS
+$(document).ready( function(){
+	$('#content').redactor();
+	habari.editor = {
+		insertSelection: function(value) {
+			$('#content').insertHtml(value);
+		}
+	}
+});
+REDACTOR_INIT_JS;
 			Stack::add( 'admin_header_javascript', $js, 'redactor_init', 'redactor' );
 		}
 	}
